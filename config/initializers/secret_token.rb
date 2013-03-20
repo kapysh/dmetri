5679107db17e55e3cb43c9f0a8a00ffef7d21fbd
admwazo <speilberg.will@gmail.com>
2013-03-20 11:19:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dmetri::Application.config.secret_token = 'fe33799673072633f958a8177e9b431f9331f8e44f541e636ecaceb290f902a546910b2926a5b9cf28cd1852ec1ccbe80a9c2068b65d8b9ce0e1a411ff6d4455'
